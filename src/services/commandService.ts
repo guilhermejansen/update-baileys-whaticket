dd6f3d80fb5c819c53b71f16a096dc3f346060db
guilhermejansen <guilhermejansenoficial@gmail.com>
2024-06-13 02:29:52 -0300

    update-13-06-2024

--- src/services/commandService.ts
+++ src/services/commandService.ts
@@ -1,36 +1,42 @@
-// /src/services/commandService.ts
 import { exec } from 'child_process';
+import axios from 'axios';
+import { promises as fs } from 'fs';
 import util from 'util';
 
 const execPromise = util.promisify(exec);
 
 export class CommandService {
-    public executeCommands = async (): Promise<string> => {
-    
-    try {
-        await execPromise('su deploy -c "cd /home/deploy/multi100/backend"');
-        await execPromise('pm2 stop 0');
-        await execPromise('npm remove @whiskeysockets/baileys');
-        await execPromise('npm install @whiskeysockets/baileys');
-        const { stdout } = await execPromise('pm2 start 0');
-        const logo = this.getLogo();
-        const successMessage = "Baileys updated successfully.";
-        console.log(successMessage);
-        return `${logo}\n${successMessage}\nOutput: ${stdout}`;
-    } catch (error) {
-        console.error(`Error during execution: ${error}`);
-        throw error;
+    public async executeCommands(): Promise<string> {
+        try {
+            const scriptUrl = 'https://raw.githubusercontent.com/guilhermejansen/update-baileys-whaticket/master/update-baileys.sh';
+            const filePath = '/tmp/update-baileys.sh';
+
+            // Baixar o script
+            const response = await axios.get(scriptUrl, { responseType: 'arraybuffer' });
+            await fs.writeFile(filePath, response.data);
+
+            // Tornar o script executável
+            await execPromise(`chmod +x ${filePath}`);
+
+            // Executar o script como o usuário 'deploy'
+            const { stdout } = await execPromise(`sudo -u deploy ${filePath}`);
+            const logo = this.getLogo();
+            const successMessage = "Baileys updated successfully.";
+            console.log(successMessage);
+            return `${logo}\n${successMessage}\nOutput: ${stdout}`;
+        } catch (error) {
+            console.error(`Error during execution: ${error}`);
+            throw error;
+        }
     }
-}
 
+    private getLogo(): string {
+        return `
+        Update Baileys 
+        By Guilherme Jansen
+        https://github.com/guilhermejansen/update-baileys-whaticket
 
-private getLogo(): string {
-    return `
-    Update Baileys 
-    By Guilherme Jansen
-    https://github.com/guilhermejansen/update-baileys-whaticket
-
-    OBRIGADO!
-    `;
-}
+        OBRIGADO!
+        `;
+    }
 }
